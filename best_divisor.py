1dbab148a55e3e3e749840d1cbab321bfff7f694
Oz <osvaldohg@gmail.com>
2016-11-29 11:40:21 -0600

    Update best_divisor.py

--- best_divisor.py
+++ best_divisor.py
@@ -1,33 +1,25 @@
 #!/bin/python
 
 import sys
-
-
 n = int(raw_input().strip())
 
-#print n
-divs=[]
-
-for i in range(1,n+1):
-    if n%i == 0:
-        divs.append(str(n/i))
-
-print divs
 #champion=[number,sum]
 champion=[0,0]
 
-for number in divs:
-    tmp=0
-    for digit in number:
-        tmp+=int(digit)
-    #print tmp    
-    if tmp > champion[1]:
-        champion[0]=number
-        champion[1]=tmp
-        #print "champion",champion
-    if tmp == champion[1]:
-        if number < champion[0]:
+for i in range(1,n+1):
+    if n%i == 0:
+        tmp=0
+        number=str(n/i)
+        for digit in number:
+            tmp+=int(digit)
+        
+        if tmp > champion[1]:
             champion[0]=number
             champion[1]=tmp
                         
+        if tmp == champion[1]:
+            if int(number) < champion[0]:
+                champion[0]=number
+                champion[1]=tmp
+
 print champion[0]

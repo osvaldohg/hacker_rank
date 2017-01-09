560dd5a519111fa260444da453f39cdd7331188e
Oz <osvaldohg@gmail.com>
2017-01-09 17:31:30 -0600

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -3,20 +3,30 @@ import string
 n=int(raw_input())
 #print size
 abc=list(string.ascii_lowercase)
-lchars=n+(n-1)
-m=lchars+(lchars-1)
-#print m, lchars
 
-for i in xrange(lchars):
-    line=""
+letters=n+(n-1)
+lchars=letters+(letters-1)
+count=1
+reverse=False
+for i in xrange(letters):
+    cadena=""
+    for j in xrange(lchars):
+        cadena+="a"
     
-    for j in range(1,lchars+1):
-        pointer=abs(j-3)
-        line=line+abc[pointer]
+    print cadena,count
     
-    print line
+    if count >= letters or reverse:
+        count-=2
+        reverse=True
+    elif not reverse:
+        count+=2
     
     
-    
+        
+        
 
-        ILVSS Dev is involved and looking at the issues
+
+
+
+
+    

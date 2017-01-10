10f482926d486e7e11f09ede16e85db79be617dc
Oz <osvaldohg@gmail.com>
2017-01-10 16:39:51 -0600

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+#Alphabet Rangoli
 import string
 n=int(raw_input())
-#print size
 abc=list(string.ascii_lowercase)
 
 letters=n+(n-1)
@@ -9,53 +9,34 @@ lchars=letters+(letters-1)
 count=1
 reverse=False
 
-def getchars(count,lchars):
+def getchars(count,lchars,hcount):
     subchars=""
-    #print "l",lchars
-    res=(lchars-1)/2
     sider=n-1
     sreverse=False
-    if count==1:
-        return "-"*res+abc[n-1]+"-"*res
-    
+        
     for i in range(1,(count+1)+(count-1)):
         if i%2==0:
             subchars+="-"
         else:
             subchars+=abc[sider]
-            if sider <= 0 or sreverse:
+            if sider > hcount and not sreverse:
+                sider-=1
+            else:
                 sider+=1
                 sreverse=True
-            elif not sreverse:
-                sider-=1
-    
+                
     if len(subchars)<lchars:
         res=(lchars-len(subchars))/2
         subchars="-"*res+subchars+"-"*res
     return subchars
         
 for i in xrange(letters):
-    cadena=""
-    sidereverse=False
-    sidecount=1
-    chars=""
-    
-    print getchars(count,lchars)
+    hcount=abs(i-n+1)
+    print getchars(count,lchars,hcount)
        
     if count >= letters or reverse:
         count-=2
         reverse=True
     elif not reverse:
         count+=2
-    
-    
-        
-        
-
-
-
-
-
-    
-
     

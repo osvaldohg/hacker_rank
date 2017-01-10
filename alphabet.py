88a61b7ab5cdc0adb7f8ca3b14ea66a3c862e56c
Oz <osvaldohg@gmail.com>
2017-01-09 18:15:43 -0600

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -8,13 +8,40 @@ letters=n+(n-1)
 lchars=letters+(letters-1)
 count=1
 reverse=False
+
+def getchars(count,lchars):
+    subchars=""
+    #print "l",lchars
+    res=(lchars-1)/2
+    sider=n-1
+    sreverse=False
+    if count==1:
+        return "-"*res+abc[n-1]+"-"*res
+    
+    for i in range(1,(count+1)+(count-1)):
+        if i%2==0:
+            subchars+="-"
+        else:
+            subchars+=abc[sider]
+            if sider <= 0 or sreverse:
+                sider+=1
+                sreverse=True
+            elif not sreverse:
+                sider-=1
+    
+    if len(subchars)<lchars:
+        res=(lchars-len(subchars))/2
+        subchars="-"*res+subchars+"-"*res
+    return subchars
+        
 for i in xrange(letters):
     cadena=""
-    for j in xrange(lchars):
-        cadena+="a"
+    sidereverse=False
+    sidecount=1
+    chars=""
     
-    print cadena,count
-    
+    print getchars(count,lchars)
+       
     if count >= letters or reverse:
         count-=2
         reverse=True
@@ -28,5 +55,7 @@ for i in xrange(letters):
 
 
 
+
+    
 
     

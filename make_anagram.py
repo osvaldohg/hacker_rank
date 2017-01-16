3ce74083863e3e6683a9182ff21194dd83b6b291
Oz <osvaldohg@gmail.com>
2017-01-16 16:14:20 -0600

    Create make_anagram.py

---
+++ make_anagram.py
@@ -0,0 +1,22 @@
+def number_needed(a, b):
+    abc=[0 for x in xrange(26)]
+    
+    for i in a:
+        apos=ord(i)-ord('a')
+        abc[apos]+=1
+    
+    for i in b:
+        bpos=ord(i)-ord('a')
+        abc[bpos]-=1
+    
+    total=0    
+    for j in abc:
+        total+=abs(j)
+        
+    return total
+            
+
+a = raw_input().strip()
+b = raw_input().strip()
+
+print number_needed(a, b)

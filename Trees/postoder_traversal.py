0544554769279ab6b8f1e9e77570590f0e2446ba
Oz <osvaldohg@gmail.com>
2017-09-01 16:45:51 -0500

    solved postoerder traversal

---
+++ Trees/postoder_traversal.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+# by oz
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def postOrder(root):
+    #Write your code here
+    if root is not None:
+        postOrder(root.left)
+        postOrder(root.right)
+        print root.data,

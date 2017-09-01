2a4e24fd79e8c72b7d9fd943dc71cf563bc191a0
Oz <osvaldohg@gmail.com>
2017-09-01 16:58:26 -0500

    create inorder traversal tree

---
+++ Trees/inorder_traversal.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+# by oz
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def inOrder(root):
+    #Write your code here
+    if root is not None:
+        inOrder(root.left)
+        print root.data,
+        inOrder(root.right)

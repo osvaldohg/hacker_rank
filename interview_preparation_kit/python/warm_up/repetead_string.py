ca2962c32489809b57e15087a48290dee29f0a84
osvaldohg <osvaldohg@gmail.com>
2018-12-02 00:15:38 +0000

    refactored solution

--- interview_preparation_kit/python/warm_up/repetead_string.py
+++ interview_preparation_kit/python/warm_up/repetead_string.py
@@ -10,25 +10,21 @@ import sys
 
 # Complete the repeatedString function below.
 def repeatedString(s, n):
+    ssize=len(s)
     count=0
-    word=0
-    size=len(s)%n
+    pcount=0
+    fulltimes=n/ssize
+    partime=n%ssize
 
-    for letter in s:
-        if letter == 'a':
-            word+=1
-
-    for i in range(0,size):
-        if letter == 'a':
+    for i in range(0,ssize):
+        if s[i] == 'a':
             count+=1
-            
-
-    if n%len(s) == 0:
-        count= word * (n/len(s))
-    else:
-        count=count + word * (n/len(s))
-
-    return count
+            if i <partime:
+                pcount+=1
+    
+    return count*fulltimes + pcount
+    
+    
 
 
 if __name__ == '__main__':
